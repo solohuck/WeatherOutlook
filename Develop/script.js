bfb49740aee4ad32bd8e7a3ad163b1eeebdec372
solo <solohuckstep@gmail.com>
2022-10-13 00:42:18 -0400

    added all info to the 5 day weather forecast cards

--- Develop/script.js
+++ Develop/script.js
@@ -81,17 +81,41 @@ var day5 = moment().add(5, "days").format("MMM DD, YYYY");
 function getUV(lat, lon) {
        
   $.ajax({
-    url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly" + "&units=imperial&appid=42d98d76405f5b8038f2ad71187af430",
+    url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly &units=imperial&appid=42d98d76405f5b8038f2ad71187af430`,
     method: "GET",
   }).then(function (response) {
   
-    var uvIndex = response.current.uvi;
-    $("#uv-index").text("UV Index:" + " " + uvIndex);
-      if (uvIndex >= 8) {
-      $("#uv-index").css("color", "red");
-      } else if (uvIndex > 4 && uvIndex < 8) {
-      $("#uv-index").css("color", "yellow");
-      } else {
-      $("#uv-index").css("color", "green");
+  var uvIndex = response.current.uvi;
+    $("#uv-index").text(`UV Index: ${uvIndex}`);
+    if (uvIndex >= 8) {
+    $("#uv-index").css("color", "red");
+    } else if (uvIndex > 4 && uvIndex < 8) {
+    $("#uv-index").css("color", "yellow");
+    } else {
+    $("#uv-index").css("color", "green");
+      
+      var cityHigh = response.daily[0].temp.max;
+      $("#high").text(`Expected high (F):${cityHigh}`);
+    $("#temp1").text(`Temp(F): ${response.daily[1].temp.max.toFixed(1)}`);
+    $("#hum1").text(`Hum: ${response.daily[1].humidity}%`);
+    var icon1 = response.daily[1].weather[0].icon;
+    $("#temp2").text(`Temp(F): ${response.daily[2].temp.max.toFixed(1)}`);
+    $("#hum2").text(`Hum: ${response.daily[2].humidity}%`);
+    var icon2 = response.daily[2].weather[0].icon;
+    $("#temp3").text(`Temp(F): ${response.daily[3].temp.max.toFixed(1)}`);
+    $("#hum3").text(`Hum: ${response.daily[3].humidity}%`);
+    var icon3 = response.daily[3].weather[0].icon;
+    $("#temp4").text(`Temp(F): ${response.daily[4].temp.max.toFixed(1)}`);
+    $("#hum4").text(`Hum: ${response.daily[4].humidity}%`);
+    var icon4 = response.daily[4].weather[0].icon;
+    $("#temp5").text(`Temp(F): ${response.daily[5].temp.max.toFixed(1)}`);
+    $("#hum5").text(`Hum: ${response.daily[5].humidity}%`);
+    var icon5 = response.daily[5].weather[0].icon;
+    
+    $("#icon1").html(`<img src="http://openweathermap.org/img/wn/${icon1}@2x.png">`);
+    $("#icon2").html(`<img src="http://openweathermap.org/img/wn/${icon2}@2x.png">`);
+    $("#icon3").html(`<img src="http://openweathermap.org/img/wn/${icon3}@2x.png">`);
+    $("#icon4").html(`<img src="http://openweathermap.org/img/wn/${icon4}@2x.png">`);
+    $("#icon5").html(`<img src="http://openweathermap.org/img/wn/${icon5}@2x.png">`);
     }}
 )}
