93220f41d4ae2149f032789e07831e1f2daaf176
solo <solohuckstep@gmail.com>
2022-10-13 00:36:34 -0400

    added UV index and 5 future dates on 5 cards

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,4 @@
+ 
 var todayDate = moment().format("MMM DD, YYYY");
 var city;
 var cities;
@@ -10,6 +11,7 @@ function loadRecentCities() {
   cities = [];
   }
 }
+loadRecentCities();
 
 $("#submit").on("click", (event) => {
   event.preventDefault();
@@ -43,6 +45,12 @@ var longLat = [];
     $("#temp").text(`Current Temp (F): ${response.main.temp.toFixed(1)}`);
     $("#humidity").text(`Humidity: ${response.main.humidity}%`);
     $("#wind-speed").text(`Wind Speed: ${response.wind.speed} mph`); 
+    $("#date1").text(day1);
+    $("#date2").text(day2);
+    $("#date3").text(day3);
+    $("#date4").text(day4);
+    $("#date5").text(day5);
+    getUV(response.coord.lat, response.coord.lon);
   });
 }
     
@@ -51,11 +59,39 @@ function loadMostRecent() {
 if (lastSearch) {
   city = lastSearch;
   search();
-} else {
+  } else {
   city = " ";
   search();
+  }
 }
+loadMostRecent();
+
+function saveToLocalStorage() {
+  localStorage.setItem("mostRecent", city);
+  cities.push(city);
+  localStorage.setItem("cities", JSON.stringify(cities));
 }
 
-loadRecentCities();
-loadMostRecent();
+var day1 = moment().add(1, "days").format("MMM DD, YYYY");
+var day2 = moment().add(2, "days").format("MMM DD, YYYY");
+var day3 = moment().add(3, "days").format("MMM DD, YYYY");
+var day4 = moment().add(4, "days").format("MMM DD, YYYY");
+var day5 = moment().add(5, "days").format("MMM DD, YYYY");
+
+function getUV(lat, lon) {
+       
+  $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly" + "&units=imperial&appid=42d98d76405f5b8038f2ad71187af430",
+    method: "GET",
+  }).then(function (response) {
+  
+    var uvIndex = response.current.uvi;
+    $("#uv-index").text("UV Index:" + " " + uvIndex);
+      if (uvIndex >= 8) {
+      $("#uv-index").css("color", "red");
+      } else if (uvIndex > 4 && uvIndex < 8) {
+      $("#uv-index").css("color", "yellow");
+      } else {
+      $("#uv-index").css("color", "green");
+    }}
+)}
